e281f6b46e9e0d1fb5356d10c4c51a680946ee93
Aaron Friedman <aaron.js.friedman@gmail.com>
2018-12-05 13:51:26 -1000

    Implement shortestPath for unweighted graphs.

--- src/test/kotlin/com/doublea/graph/GraphTest.kt
+++ src/test/kotlin/com/doublea/graph/GraphTest.kt
@@ -1,7 +1,9 @@
 package com.doublea.graph
 
 import io.kotlintest.shouldBe
+import io.kotlintest.shouldThrow
 import io.kotlintest.specs.StringSpec
+import java.lang.RuntimeException
 
 
 internal class AdjacencyMatrixGraphTest : StringSpec({
@@ -94,6 +96,46 @@ internal class AdjacencySetGraphTest : StringSpec({
         graph.addEdge(2, 3)
         graph.addEdge(3, 4)
         graph.isConnected() shouldBe true
+    }
+
+    "shortestPath returns list of shortest path from one vertex to another" {
+        val graph = AdjacencySetGraph(5, GraphType.DIRECTED)
+        graph.addEdge(0, 2)
+        graph.addEdge(1, 3)
+        graph.addEdge(2, 4)
+        graph.addEdge(4, 1)
+
+        graph.shortestPath(0, 3) shouldBe listOf(0, 2, 4, 1, 3)
+        graph.shortestPath(1, 3) shouldBe listOf(1, 3)
+        graph.shortestPath(2, 1) shouldBe listOf(2, 4, 1)
+
+        graph.addEdge(0, 1)
+
+        graph.shortestPath(0, 3) shouldBe listOf(0, 1, 3)
+        graph.shortestPath(2, 1) shouldBe listOf(2, 4, 1)
+    }
+
+    "shortestPath works for a graph with cycles" {
+        val graph = AdjacencySetGraph(5, GraphType.UNDIRECTED)
+        graph.addEdge(0, 2)
+        graph.addEdge(1, 3)
+        graph.addEdge(2, 4)
+        graph.addEdge(4, 1)
+
+        graph.shortestPath(0, 3) shouldBe listOf(0, 2, 4, 1, 3)
+        graph.shortestPath(1, 3) shouldBe listOf(1, 3)
+        graph.shortestPath(2, 1) shouldBe listOf(2, 4, 1)
+    }
+
+    "shortestPath throws a RuntimeException when there is no connection from one vertex to another" {
+        val graph = AdjacencySetGraph(5, GraphType.DIRECTED)
+        graph.addEdge(0, 2)
+        graph.addEdge(2, 4)
+        graph.addEdge(4, 1)
+        shouldThrow<RuntimeException> {
+            graph.shortestPath(2, 0)
+        }
+
     }
 })
 
